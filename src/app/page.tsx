679a6d142d957ea691f0a4cb9abe0921c3f3492b
David Mihok <DavidMihok@s.zochova.sk>
2024-09-12 10:24:13 +0200

    uvodny commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Typography from '@mui/material/Typography';
+export default function Types() {
+  return (
+    <Typography variant="h1" gutterBottom>
+    hello world!
+  </Typography>
+  )}
