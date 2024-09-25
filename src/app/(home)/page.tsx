d846dacfc7559524075fe5d67eb53e735407faf2
David Mihok <DavidMihok@s.zochova.sk>
2024-09-25 08:41:10 +0200

    hura

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+// src/app/(home)/page.tsx
+import Typography from '@mui/material/Typography';
+
+export const metadata = {title: 'Domov | Zoškasnap'};
+
+export default function Home() {
+  return (
+    <Typography>Domovská stránka</Typography>
+  );
+}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Typography from '@mui/material/Typography';
-export default function Types() {
-  return (
-    <Typography variant="h1" gutterBottom>
-    hello world!
-  </Typography>
-  )}
